c0032050512853a07a47e3700bf839d098abe651
Ben Parisot <me@benparisot.com>
2019-02-08 11:21:18 -0800

    clean up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const numArray = [23, 21, 53, 48, 4, 16, 23];
 lowSum();
 function lowSum() {
+    const numArray = [23, 21, 53, 48, 4, 16, 23];
     numArray.sort(function(a,b) {
         return a - b;
     });
